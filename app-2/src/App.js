0136d19efe7faac0a93a4af5872b43eed1bc762f
jacoborbach <jacoborbach96@gmail.com>
2020-12-16 16:20:57 -0500

    finished 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myArr: ['spaghetti', 'meatballs', 'pepporoni', 'basil!']
+    }
+  }
+
+  render() {
+    const mapArr = this.state.myArr.map((element, i) => (
+      <h2 key={i}>{element}</h2>
+    ))
+
+    return (
+      <div className="App">
+        {mapArr}
+      </div>
+    );
+  }
 }
 
 export default App;
